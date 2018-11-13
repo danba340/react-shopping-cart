636579d3fc31aba280b9204c7eb4a63572aebf0c
Daniel Bark <danba340@gmail.com>
2018-11-13 13:02:54 +0100

    Added package enzyme. Added test for opening modal.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should set state.showModal to true on clicking CartButton', () => {
+  const app = shallow(<App />);
+  const cartButton = app.find('CartButton');
+  
+  expect(app.state().showModal).toEqual(false); 
+
+  cartButton.simulate('click');
+  expect(app.state().showModal).toEqual(true);
+
 });
